7b0ca0427073f677ee529d53c484cc7d394b83cc
amparo1206 <amparo.frontend@gmail.com>
2022-03-11 00:54:56 +0100

    README.md changes

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,6 +1,11 @@
+import './NotFound.scss'
+import death from '../../img/404dead.jpeg'
 const NotFound = () => {
-    return (
-      <div className="Error404">404 Page not found</div>
+  return (
+
+    <div className="Error404">404 Page not found
+      <img src={death}/>
+    </div>
     )
   }
   
